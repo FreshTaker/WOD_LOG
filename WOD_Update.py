f0404a3b78297e3b720f8ac2a810cc497e6f7a00
PBrockmann <pjbrockmann@gmail.com>
2020-07-23 13:19:59 -0400

    Making progress on Update

--- WOD_Update.py
+++ WOD_Update.py
@@ -6,4 +6,36 @@ Script: WOD_Update.py
 
 This script adds a log entry to the WOD_LOG.
 
+Each row is a workout logged:
+[date, name, type, total rounds, total time, total reps, [workout movements, reps]]
+* Function will need to search and add columns to DF if not present already.
+
+
 """
+import datetime
+import pandas
+
+def get_CSV(FILENAME):
+    """ Retrieves CSV file containing workout log information. """
+    LOGFILE = pandas.read_csv(FILENAME)
+    return LOGFILE
+
+def add_workout(type):
+    print("")
+
+print("WOD_Update.py started.")
+USER_N = input("Enter USER NUMBER: ")
+filename = 'WOD_LOG_' + str(USER_N) + '.csv'
+
+try:
+    userlog = get_CSV(filename)
+except Exception as e:
+    print(type(e))
+    print(e.args)
+    print(e)
+
+
+print(userlog)
+
+
+
